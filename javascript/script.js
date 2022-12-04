f817fa858333e62137fd36445dfa09f34f9c36e4
grafondato <gracielafondato@gmail.com>
2022-12-03 23:55:24 -0300

    Proyecto Final

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/*consultar si es mayor de edad para ingresar*/
+let consulta = prompt ("¿cuantos años tienes?");
+if (consulta >= 18){
+    alert("Bienvenido a Velvet ¿Listo para potenciar tu belleza?");
+} else {
+    alert("No puedes ingresar");
+}
